854a581b953725367d9c63be99fe295f70865b18
Janine Luk <janineluk95@gmail.com>
2018-11-17 12:32:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,9 +16,7 @@ def first_challenge
   #your code here
   
   contacts.each do |person, data|
-  
   data.each do |attribute, value|
-    
   if attribute == :favorite_icecream_flavors
   value.delete_if |flavor| flavor == "strawberry"
 end

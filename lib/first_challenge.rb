45fcf8c05274d5363161807a2f4a62d440d97789
Janine Luk <janineluk95@gmail.com>
2018-11-17 12:26:25 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,9 @@ def first_challenge
 
   #your code here
   
-  contacts.each do |
+  contacts.each do |person, data|
+  
+  values.each do |
   
   #remember to return your newly altered contacts hash!
   contacts
